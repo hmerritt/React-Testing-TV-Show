e931fc2bb6fed96e29f5abe2069753edfe8c83c9
Harry Merritt <harry@merritt.es>
2020-03-12 02:02:55 +0000

    Attempt to get more tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,236 @@
 import React from "react";
-import { render, fireEvent, wait } from "@testing-library/react";
-//import { fetchShow } from "../api/fetchShow";
+import { render, fireEvent, wait, act } from "@testing-library/react";
+import { fetchShow as mockFetchShow } from "../api/fetchShow";
 
 import App from "../App";
 
-//jest.mock("../api/fetchShow");
+const shows = [
+    {
+        id: 2993,
+        url: "http://www.tvmaze.com/shows/2993/stranger-things",
+        name: "Stranger Things",
+        type: "Scripted",
+        language: "English",
+        genres: ["Drama", "Fantasy", "Science-Fiction"],
+        status: "Running",
+        runtime: 60,
+        premiered: "2016-07-15",
+        officialSite: "https://www.netflix.com/title/80057281",
+        schedule: {
+            time: "",
+            days: ["Thursday"]
+        },
+        rating: {
+            average: 8.7
+        },
+        weight: 99,
+        network: null,
+        webChannel: {
+            id: 1,
+            name: "Netflix",
+            country: null
+        },
+        externals: {
+            tvrage: 48493,
+            thetvdb: 305288,
+            imdb: "tt4574334"
+        },
+        image: {
+            medium:
+                "http://static.tvmaze.com/uploads/images/medium_portrait/200/501942.jpg",
+            original:
+                "http://static.tvmaze.com/uploads/images/original_untouched/200/501942.jpg"
+        },
+        summary:
+            "<p>A love letter to the '80s classics that captivated a generation, <b>Stranger Things</b> is set in 1983 Indiana, where a young boy vanishes into thin air. As friends, family and local police search for answers, they are drawn into an extraordinary mystery involving top-secret government experiments, terrifying supernatural forces and one very strange little girl.</p>",
+        updated: 1576977557,
+        _links: {
+            self: {
+                href: "http://api.tvmaze.com/shows/2993"
+            },
+            previousepisode: {
+                href: "http://api.tvmaze.com/episodes/1576476"
+            }
+        },
+        _embedded: {
+            episodes: [
+                {
+                    id: 553946,
+                    url:
+                        "http://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
+                    name: "Chapter One: The Vanishing of Will Byers",
+                    season: 1,
+                    number: 1,
+                    airdate: "2016-07-15",
+                    airtime: "",
+                    airstamp: "2016-07-15T12:00:00+00:00",
+                    runtime: 60,
+                    image: {
+                        medium:
+                            "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+                        original:
+                            "http://static.tvmaze.com/uploads/images/original_untouched/67/168918.jpg"
+                    },
+                    summary:
+                        "<p>A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.</p>",
+                    _links: {
+                        self: {
+                            href: "http://api.tvmaze.com/episodes/553946"
+                        }
+                    }
+                },
+                {
+                    id: 578663,
+                    url:
+                        "http://www.tvmaze.com/episodes/578663/stranger-things-1x02-chapter-two-the-weirdo-on-maple-street",
+                    name: "Chapter Two: The Weirdo on Maple Street",
+                    season: 1,
+                    number: 2,
+                    airdate: "2016-07-15",
+                    airtime: "",
+                    airstamp: "2016-07-15T12:00:00+00:00",
+                    runtime: 60,
+                    image: {
+                        medium:
+                            "http://static.tvmaze.com/uploads/images/medium_landscape/72/181376.jpg",
+                        original:
+                            "http://static.tvmaze.com/uploads/images/original_untouched/72/181376.jpg"
+                    },
+                    summary:
+                        "<p>While the search for the missing Will continues, Joyce tells Jim about a call she apparently received from her son. Meanwhile, Jane warns Mike that there are bad people after her, and he realizes that she knows what happened to Will.</p>",
+                    _links: {
+                        self: {
+                            href: "http://api.tvmaze.com/episodes/578663"
+                        }
+                    }
+                },
+                {
+                    id: 578664,
+                    url:
+                        "http://www.tvmaze.com/episodes/578664/stranger-things-1x03-chapter-three-holly-jolly",
+                    name: "Chapter Three: Holly, Jolly",
+                    season: 1,
+                    number: 3,
+                    airdate: "2016-07-15",
+                    airtime: "",
+                    airstamp: "2016-07-15T12:00:00+00:00",
+                    runtime: 60,
+                    image: {
+                        medium:
+                            "http://static.tvmaze.com/uploads/images/medium_landscape/67/168920.jpg",
+                        original:
+                            "http://static.tvmaze.com/uploads/images/original_untouched/67/168920.jpg"
+                    },
+                    summary:
+                        "<p>While Nancy looks for a missing Barbara and realizes that Jonathan may have been the last person to see her, Mike and his friends go out with Jane to find the missing Will. Meanwhile, Jim tracks Will to the lab.</p>",
+                    _links: {
+                        self: {
+                            href: "http://api.tvmaze.com/episodes/578664"
+                        }
+                    }
+                },
+                {
+                    id: 909340,
+                    url:
+                        "http://www.tvmaze.com/episodes/909340/stranger-things-2x01-chapter-one-madmax",
+                    name: "Chapter One: MADMAX",
+                    season: 2,
+                    number: 1,
+                    airdate: "2017-10-27",
+                    airtime: "",
+                    airstamp: "2017-10-27T12:00:00+00:00",
+                    runtime: 60,
+                    image: {
+                        medium:
+                            "http://static.tvmaze.com/uploads/images/medium_landscape/132/331976.jpg",
+                        original:
+                            "http://static.tvmaze.com/uploads/images/original_untouched/132/331976.jpg"
+                    },
+                    summary:
+                        "<p>One year after the events with the Upside Down and the Demogorgon, Will meets with a government doctor. The boys discover that there's a new player in town, and Jim pays a visit to El.</p>",
+                    _links: {
+                        self: {
+                            href: "http://api.tvmaze.com/episodes/909340"
+                        }
+                    }
+                },
+                {
+                    id: 909342,
+                    url:
+                        "http://www.tvmaze.com/episodes/909342/stranger-things-2x02-chapter-two-trick-or-treat-freak",
+                    name: "Chapter Two: Trick or Treat, Freak",
+                    season: 2,
+                    number: 2,
+                    airdate: "2017-10-27",
+                    airtime: "",
+                    airstamp: "2017-10-27T12:00:00+00:00",
+                    runtime: 60,
+                    image: {
+                        medium:
+                            "http://static.tvmaze.com/uploads/images/medium_landscape/132/332034.jpg",
+                        original:
+                            "http://static.tvmaze.com/uploads/images/original_untouched/132/332034.jpg"
+                    },
+                    summary:
+                        "<p>The boys go trick-or-treating on Halloween, and Will has another vision. Meanwhile, El relieves the days following her escape from the Upside Down, and Dustin finds something in the garbage can.</p>",
+                    _links: {
+                        self: {
+                            href: "http://api.tvmaze.com/episodes/909342"
+                        }
+                    }
+                },
+                {
+                    id: 1576469,
+                    url:
+                        "http://www.tvmaze.com/episodes/1576469/stranger-things-3x01-chapter-one-suzie-do-you-copy",
+                    name: "Chapter One: Suzie, Do You Copy?",
+                    season: 3,
+                    number: 1,
+                    airdate: "2019-07-04",
+                    airtime: "",
+                    airstamp: "2019-07-04T12:00:00+00:00",
+                    runtime: 51,
+                    image: {
+                        medium:
+                            "http://static.tvmaze.com/uploads/images/medium_landscape/204/510098.jpg",
+                        original:
+                            "http://static.tvmaze.com/uploads/images/original_untouched/204/510098.jpg"
+                    },
+                    summary:
+                        "<p>Things change over the summer: Jonathan, Nancy, Steve, and Billy get jobs; Dustin goes to science camp; El and Mike become an item; Lucas and Max almost become an item. Meanwhile, mysterious power outages plague Hawkins and rats start exploding.</p>",
+                    _links: {
+                        self: {
+                            href: "http://api.tvmaze.com/episodes/1576469"
+                        }
+                    }
+                },
+                {
+                    id: 1752754,
+                    url:
+                        "http://www.tvmaze.com/episodes/1752754/stranger-things-4x01-chapter-one-the-hellfire-club",
+                    name: "Chapter One: The Hellfire Club",
+                    season: 4,
+                    number: 1,
+                    airdate: "",
+                    airtime: "",
+                    airstamp: null,
+                    runtime: 60,
+                    image: null,
+                    summary: null,
+                    _links: {
+                        self: {
+                            href: "http://api.tvmaze.com/episodes/1752754"
+                        }
+                    }
+                }
+            ]
+        }
+    }
+];
 
-const shows = {
-    "Season 1": [
-        {}
-    ]
-};
+// jest.mock("../api/fetchShow");
+// mockFetchShow.mockResolvedValueOnce(shows);
+
 
 test("renders App without crashing", () => {
     render(<App />);
@@ -24,9 +244,32 @@ test("renders loading text and content after api call", async () => {
     findAllByText(/Stranger Things/i);
     findAllByText(/Select a season/i);
 });
-//
-// test("renders App properly", async () => {
-//     const { getByText, getByTestId } = render(<App />);
+
+
+// test("renders loading text and content after api call", async () => {
+//     const { getByText, findByText, findAllByText } = render(<App />);
 //
 //     getByText(/Fetching data.../i);
+//     await wait();
+//
+//     fireEvent.click(getByText(/Select a season/i));
+//     await wait();
+//     fireEvent.click(getByText("Season 1"));
+//     await wait();
+//
+//     findByText(/Chapter One: The Vanishing of Will Byers/i);
+//
+// });
+
+// test("renders seasons as expected", async () => {
+//     const { getByText, findByText, findAllByText } = render(<App />);
+//
+//     await wait();
+//     fireEvent.click(getByText("Select a season"));
+//     fireEvent.click(getByText("Season 2"));
+//     await wait();
+//
+//     findByText(/Chapter One: The Vanishing of Will Byers/i);
+//     findByText(/Chapter Two: The Weirdo on Maple Street/i);
+//     findByText(/Chapter Three: Holly, Jolly/i);
 // });
---
+++ src/tests/Episodes.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, fireEvent, wait, act } from "@testing-library/react";
+
+import Episodes from "../components/Episodes";
+
+// test("renders Episodes without crashing", () => {
+//     render(<Episodes />);
+// });
